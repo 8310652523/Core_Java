a2b7b0b5346595f988543fe13016440f14516773
B Radha <radhabellary0305@gmail.com>
2026-02-16 21:50:14 +0530

    Add files via upload

---
+++ variables1.java
@@ -0,0 +1,73 @@
+class Variables1
+{
+	public static void main(String[] arg){
+		String stdname="ramesh";
+		char sec='A';
+		double fee=34000.50;
+		String in_name="jspider";
+		System.out.println("institute name is " + in_name);
+		
+		double mobile_cost=99999.9;
+		String model_name="iphone";
+		String color="white";
+		System.out.println("The mobile cost is " + mobile_cost);
+		System.out.println("The model name is " + model_name);
+		System.out.println("The mobile color is " + color);
+		
+		double car_cost=1000000.00;
+		String mode_name="maruti suzuki";
+		String type="diesel";
+		System.out.println("The price of car is " + mode_name);
+		System.out.println("The mode name is " + mode_name);
+		System.out.println("The car type is " + type);
+		
+		String school_name="Lions eng med high school";
+		char school_grade='A';
+		int school_strength=300;
+		System.out.println("The school name is " + school_name);
+		System.out.println("The school grade is " + school_grade);
+		System.out.println("The school strength is " + school_strength);
+		
+		double tv_cost=20000.00;
+		String tv_brand="LG";
+		String tv_type="smart tv";
+		System.out.println("The tv cost is " + tv_cost);
+		System.out.println("The tv brand is " + tv_brand);
+		System.out.println("The tv type is " + tv_type);
+		
+		int bike_cost=70000;
+		String bike_brand="duke";
+		String bike_color="orange";
+		System.out.println("The bike cost is " + bike_cost);
+		System.out.println("The bike brand is " + bike_brand);
+		System.out.println("The bike color is " + bike_color);
+		
+		String course_name="java";
+		String univ_name="VTU";
+		int exam_cost=500;
+		System.out.println("The course name is " + course_name);
+		System.out.println("The university name is " + univ_name);
+		System.out.println("The exam cost is " + exam_cost);
+		
+		String lap_name="asus vivobook";
+		int cost=55000;
+		String brand="vivobook";
+		System.out.println("The laptop name is " + lap_name);
+		System.out.println("The laptop cost is " + cost);
+		System.out.println("The laptop brand is " + brand);
+		
+		String home_name="NY Nilaya";
+		double home_cost=4000000.00;
+		String home_color="pink";
+		System.out.println("The home name is " + home_name);
+		System.out.println("The home cost is " + home_cost);
+		System.out.println("The home color is " + home_color);
+		
+		int emp_id=1;
+		int emp_sal=50000;
+		char emp_grade='A';
+		System.out.println("The employee id is " + emp_id);
+		System.out.println("The employee salary is " + emp_sal);
+		System.out.println("The employee grade is " + emp_grade);
+	}
+}
