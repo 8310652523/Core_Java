a4a362177db63faba9ea681ea1a5971b0db20297
B Radha <radhabellary0305@gmail.com>
2026-02-16 21:49:00 +0530

    Add files via upload

---
+++ Sample1.java
@@ -0,0 +1,10 @@
+class Sample1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Father name is : Nagaraj");
+		System.out.println("Mother name is : Yallamma");
+		System.out.println("Sister name is : Rekha");
+		System.out.println("Brother name is : Ravi");
+	}
+}
---
+++ variables.java
@@ -0,0 +1,7 @@
+class variables{
+	public static void main(String[] args){
+		int a;
+		a=10;
+		System.out.println(a);
+	}
+}
